00f493f4f96d21137c70af038ecd6d35cc200104
rapa0714 <rapa0714@naver.com>
2022-06-01 23:21:21 +0900

    1

---
+++ bmi,bmrcomponent/src/App.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+// App Components
+import InfoComponent from "./components/InfoComponent";
+import ResultButtonComponent from "./components/ResultButtonComponent";
+import CalResultComponent from "./components/CalResultComponent";
+
+function App() {
+  const [resultUI, setResultUI] = useState(false);
+
+  const [gender, setGender] = useState(false);
+  const [age, setAge] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+
+  function Modal() {
+    return (
+      <CalResultComponent
+        age={age}
+        height={height}
+        weight={weight}
+        gender={gender}
+      />
+    );
+  }
+
+  useEffect(() => {
+    console.log(age, height, weight, gender);
+  }, [age, height, weight, gender]);
+
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="title-nav">
+          <h1>건강 지킴이</h1>
+        </div>
+        <br /> <br />
+        <InfoComponent
+          setGender={setGender}
+          setAge={setAge}
+          setHeight={setHeight}
+          setWeight={setWeight}
+        />
+        <div className="result-view">
+          <br />
+          {/* 버튼 클릭 시 결과 UI가 열리고 닫힘 */}
+          <button
+            className="resultBtn"
+            onClick={() => {
+              setResultUI(!resultUI);
+            }}
+          >
+            결과보기
+          </button>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+
+      {/* 결과컴포넌트 on/off */}
+      {resultUI ? <Modal /> : null}
+    </div>
+  );
+}
+
+export default App;
---
+++ bmi,bmrcomponent/src/components/InfoComponent.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import "../App.js";
+import "../App.css";
+
+const InfoComponent = ({ setAge, setHeight, setWeight, setGender }) => {
+  const onChangeAge = (e) => {
+    setAge(e.target.value);
+  };
+  const onChangeHeight = (e) => {
+    setHeight(e.target.value);
+  };
+  const onChangeWeight = (e) => {
+    setWeight(e.target.value);
+  };
+
+  return (
+    <div className="info-form">
+      <form>
+        <br />
+        <p />
+        <label>▶ 성별 &nbsp;</label>
+        <input
+          onClick={() => {
+            setGender(false);
+          }}
+          type="radio"
+          id="gender"
+          name="gender"
+          value="female"
+        ></input>
+        여성 &nbsp;
+        <input
+          onClick={() => {
+            setGender(true);
+          }}
+          type="radio"
+          id="gender"
+          name="gender"
+          value="male"
+        ></input>
+        남성
+        <img
+          src="images/turtle.png"
+          alt=""
+          align="right"
+          width="200px"
+          height="200px"
+        />
+        <p />
+        <label>▶ 나이 &nbsp;</label>
+        <input onChange={onChangeAge} placeholder="0" />
+        <p />
+        <label>▶ 신장 &nbsp;</label>
+        <input onChange={onChangeHeight} placeholder="0" />
+        <p />
+        <label>▶ 체중 &nbsp;</label>
+        <input onChange={onChangeWeight} placeholder="0" />
+        <p />
+      </form>
+    </div>
+  );
+};
+
+export default InfoComponent;
